39d10e38c9c837a37aaf4e78ff5f0543ac8a606b
潘芊寧 <B12705005@ntu.edu.tw>
2025-12-19 06:43:39 +0000

    feat: add /health endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const app = require("./app");
 const PORT = 3000;
+app.get('/health', (req, res) => res.status(200).send('ok'));
 
 // Start the server
 const server = app.listen(PORT, () => {
