7b5b25b23fe554fc9ec356dd858c107d1d8383f9
潘芊寧 <B12705005@ntu.edu.tw>
2025-12-20 10:26:26 +0000

    demo: update homepage text on dev

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req, res) => {
   res
     .status(200)
-    .send("<h1>Welcome to the CI/CD Workshop! (DEV DEMO 10:18:16)</h1>");
+    .send("<h1>Welcome to the CI/CD Workshop! (DEV DEMO 10:26:15) (DEV DEMO 10:18:16)</h1>");
 });
 
 module.exports = app;
